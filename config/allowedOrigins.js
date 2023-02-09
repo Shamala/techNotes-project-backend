11c80cb9fb9a1e1c7a75e8cbba103e5dd02bea40
Shamala <msurabhi23@gmail.com>
2023-02-09 15:11:12 -0800

    updatedwq

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  //   "http://localhost:3500",
-  //   "http://localhost:3000",
+  // "http://localhost:3500",
+  // "http://localhost:3000",
   "https://technotes-y4sb.onrender.com", //deployed on render.com
 ];
 
